1abe83ca0e8e81ce024dd0b8e3ff44a02c5300d4
MeherSaiG <mehersaig40aiml@gmail.com>
2026-06-10 00:24:44 +0530

    Update mysql_service.py

--- mysql_service.py
+++ mysql_service.py
@@ -4,7 +4,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="12345678",
+    password="your_password",
     database="Citizengrivance"
 )
 
